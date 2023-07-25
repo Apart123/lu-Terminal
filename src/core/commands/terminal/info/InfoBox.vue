a09c8e023cb1a15d819c32731847e7fc7c8131cb
luying <18280977332@163.com>
2023-07-26 00:35:09 +0800

    commit

---
+++ src/core/commands/terminal/info/InfoBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>关于本站：LuIndex 终端式的浏览器主页</div>
+  <div>
+    作者：
+    <a href="#" target="_blank">
+      luying
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface InfoBoxProps {
+  seconds: string;
+}
+
+const props = withDefaults(defineProps<InfoBoxProps>(), {});
+</script>
+
+<style scoped></style>
